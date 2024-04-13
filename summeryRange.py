ff1be1ac16a0271695dc9648a1df5c740b27f841
cnaf254 <eshetugezu16@gmail.com>
2024-04-13 14:26:34 +0300

    summery range

---
+++ summeryRange.py
@@ -0,0 +1,12 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if not nums:
+            return []
+
+        result, start = [], nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1] + 1:
+                result.append(f"{start}->{nums[i - 1]}" if start != nums[i - 1] else str(start))
+                start = nums[i]
+        result.append(f"{start}->{nums[-1]}" if start != nums[-1] else str(start))
+        return result
