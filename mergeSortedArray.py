5185e93c9b8f6a75c614f0ab5e203d5fdf98a78b
cnaf254 <eshetugezu16@gmail.com>
2024-06-25 16:22:29 +0300

    merge sorted array

---
+++ mergeSortedArray.py
@@ -0,0 +1,9 @@
+def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+    while m > 0 and n > 0:
+        if nums1[m-1] > nums2[n-1]:
+            nums1[m+n-1] = nums1[m-1]
+            m -= 1
+        else:
+            nums1[m+n-1] = nums2[n-1]
+            n -= 1
+    nums1[:n] = nums2[:n]
