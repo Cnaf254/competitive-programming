7adbb679eaf356b08e3c01e46fd42fad3fc5690e
cnaf254 <eshetugezu16@gmail.com>
2024-08-19 11:42:29 +0300

    teemo attacking

---
+++ teemoAttacking.py
@@ -0,0 +1,5 @@
+def findPoisonedDuration(timeSeries: List[int], duration: int) -> int:
+    total_time = 0
+    for i in range(len(timeSeries) - 1):
+        total_time += min(timeSeries[i + 1] - timeSeries[i], duration)
+    return total_time + duration
